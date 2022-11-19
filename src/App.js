467c96056f4c6253079c6a097c99a83f429c83a0
smalltimebenny <ben.lindquist@gmail.com>
2022-11-19 14:42:41 -0500

    assignment done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import {BrowserRouter, Routes, Route, Link, redirect, useParams} from 'react-router-dom'
 import './App.css';
 
+const Blank = (props) => {
+	return redirect("/home");
+}
+
+const Home = (props) => {
+	return (
+		<>
+			<h1>Welcome!</h1>
+		</>
+		)
+	
+}
+
+const Entry = (props) => {
+	const entry =useParams();
+	
+	let result = (data) => {return isNaN(data) ? `The word is: ${data}.` : `The number is: ${data}.`};
+	
+	return <h1>{result(entry.entry)}</h1>
+}
+
+const Colors = (props) => {
+	const {coler} =useParams();
+	const {colur} =useParams();
+	const {worrd} =useParams();
+	
+	return <h1 style = {{backgroundColor: `${colur}`, color:`${coler}`}}>{worrd}</h1>;
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+		<div className="App">
+			<Routes>
+				<Route path="/" element={<Blank />} />
+				<Route path="/home" element={<Home />} />
+				<Route path="/:entry" element={<Entry />} />
+				<Route path="/:worrd/:coler/:colur" element={<Colors />}/>
+			</Routes>
+			
+		</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
